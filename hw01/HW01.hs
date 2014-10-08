23b94fd2ba2b3aee31416a0e83e7efbd65280161
Ronnie Howell <ronniemhowell@gmail.com>
2014-10-07 23:03:47 -0400

    Week 1 - Exercise 1 through 4

---
+++ hw01/HW01.hs
@@ -0,0 +1,48 @@
+{-
+Name: Ronnie Howell 
+Collaborators: <"none">
+Notes: doubleEveryOther was a pain in the ass
+-}
+
+module HW01 where         -- We'll learn more about this later
+import Data.List
+
+isThisWorking :: String
+isThisWorking = "Yes"
+-- Load this file into GHCi (say, with `ghci HW01.hs`) and type
+-- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
+
+-- Put your work below.
+lastDigit :: Integer -> Integer
+lastDigit x = mod x 10
+
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit x = div x 10
+
+toDigits :: Integer -> [Integer]
+toDigits x | x <= 0 = []
+           | x >  0 = toDigitsInner (dropLastDigit x) [lastDigit x]
+
+toDigitsInner :: Integer -> [Integer] -> [Integer]
+toDigitsInner 0 xs = xs
+toDigitsInner x xs = 
+  toDigitsInner (dropLastDigit x) (lastDigit x : xs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []                                                      -- Empty
+doubleEveryOther (x:[]) = [x]                                                 -- One Element
+doubleEveryOther (x:y:xs) = (x * 2) : y : doubleEveryOther(xs)
+
+
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum (expandDigits xs)
+
+expandDigits :: [Integer] -> [Integer]
+expandDigits [] = []
+expandDigits (x:xs) = (toDigits x) ++ (expandDigits xs)
+
+validate :: Integer -> Bool
+validate x = (sumDigits (expandDigits (doubleEveryOther (toDigits x)))) `mod` 10 == 0 
+
+--
